5612465383829dbd9bf895b1f5931ce686050255
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 21:01:24 -0500

    Update snmp_client.sh

--- snmp_client.sh
+++ snmp_client.sh
@@ -35,7 +35,7 @@ for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{p
 	esac
 	echo "$XTIME up $UPTIME, 0 users, load=$LOAD%" >> $MSG
 	$BB $BBDISP "@" < $MSG
- 	
+ 	rm $MSG
 #	echo "\tSensor Name\t\tCelsius\tFahrenheit" > /tmp/$HOSTNAME.temp
 #	for TARGET in $(snmpwalk -Oq -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.101.4.3 | grep -i tempera | awk '{print $1}' ); do
 #		SENSOR=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET | sed 's/"//g' | sed 's/ /_/g')
