6b6378168f2e35d0de066271adfc0c099ca94a62
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 21:38:44 -0500

    Update snmp_client.sh

--- snmp_client.sh
+++ snmp_client.sh
@@ -36,20 +36,5 @@ for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{p
 	echo "$XTIME up $UPTIME, 0 users, load=$LOAD%" >> $MSG
 	$BB $BBDISP "@" < $MSG
  	rm $MSG
-#	echo "\tSensor Name\t\tCelsius\tFahrenheit" > /tmp/$HOSTNAME.temp
-#	for TARGET in $(snmpwalk -Oq -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.101.4.3 | grep -i tempera | awk '{print $1}' ); do
-#		SENSOR=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET | sed 's/"//g' | sed 's/ /_/g')
-#		if [ "$SENSOR" = "" ]; then
-#			break
-#		fi
-#		TARGET2=$(echo $TARGET | sed 's/.101.4.3.2.1.2./.101.4.3.2.1.3./g')
-#		TEMPERATURE=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME $TARGET2 | sed 's/"//g')
-#		TEMPERATURE2=$(echo "scale=1; $TEMPERATURE/1" | bc)
-#		TEMPERATUREF=$(echo "scale=1; $TEMPERATURE2*1.8+32" | bc)
-#		echo "&green\t$SENSOR\t$TEMPERATURE2\t$TEMPERATUREF" >> /tmp/$HOSTNAME.temp
-#	done
-#	$BB $BBDISP "status $HOSTNAME.temperature green `date`
-#`cat /tmp/$HOSTNAME.temp`
-#"
 done
 
