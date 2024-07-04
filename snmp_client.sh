e58a8caef441f5558fd403ce05c2fc234dc8b4ec
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 20:45:11 -0500

    Update snmp_client.sh

--- snmp_client.sh
+++ snmp_client.sh
@@ -13,7 +13,7 @@ for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{p
 	date >> $MSG
 	echo "[uptime]" >> $MSG
 	XTIME=$(uptime | awk '{print $1}')
- 	case "$(HOSTNAME)" in
+ 	case "$HOSTNAME" in
   		*fw*)
     			LOAD=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME .1.3.6.1.4.1.12356.101.4.1.3.0)
        			;;
