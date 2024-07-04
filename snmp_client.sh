340e6284da2e85e5d114d71528c71ab114828054
Bill <BobcatBill@users.noreply.github.com>
2024-07-03 20:56:15 -0500

    Update snmp_client.sh

--- snmp_client.sh
+++ snmp_client.sh
@@ -4,8 +4,8 @@ SNMPSTRING=public
 for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
 	UPTIME=$(snmpget -Ov -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.2.1.1.3.0 | awk '{printf($3" "$4" "$5)}')
  	if [ "$UPTIME" = "" ]; then
-		echo "Error on host $HOSTNAME
-  		exit 1
+		echo "Error on host $HOSTNAME"
+ 		exit 1
 	fi
 	MSG=/tmp/${HOSTNAME}.MSG.out
 	echo "client $HOSTNAME.linux linux"  >  $MSG
