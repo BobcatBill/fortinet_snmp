9331e561d24f969ba4ac7c733c32f3bce185180f
Bill <BobcatBill@users.noreply.github.com>
2024-07-05 15:06:43 -0500

    Update snmp_client.sh
    Added AP support

--- snmp_client.sh
+++ snmp_client.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+DEBUG=0
+
+if [ "$DEBUG" = "1" ]; then
+	echo "Debug enabled"
+	BB=/usr/lib/xymon/server/bin/xymon
+	BBDISP=127.0.0.1
+fi
+
 SNMPSTRING=public
 
-for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
+for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac|ap0" /etc/xymon/hosts.cfg | awk '{print $2}'`; do
 	UPTIME=$(snmpget -Ov -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.2.1.1.3.0 2>/dev/null | awk '{printf($3" "$4" "$5)}')
  	if [ "$UPTIME" = "" ]; then
 		echo "Error on host $HOSTNAME"
@@ -32,9 +40,24 @@ for HOSTNAME in `grep -E "fw|sw0|faz|fmg|fsa|fac" /etc/xymon/hosts.cfg | awk '{p
 	  	*fac*)
     			LOAD=$(snmpget -Oqv -v2c -c $SNMPSTRING $HOSTNAME 1.3.6.1.4.1.12356.113.1.4.0)
        			;;
+		*ap0*)
+			FWHOST=$(echo "$HOSTNAME" | sed 's/ap0./fw01/g')
+			SHORTHOST=$(echo $HOSTNAME | awk -F\. '{print $1}')
+			TARGET=$(snmpwalk -Oq -v2c -c $SNMPSTRING $FWHOST 1.3.6.1.4.1.12356.101.14.4.3.1.3 | grep $SHORTHOST | awk '{print $1}')
+			echo "TARGET=$TARGET"
+			TARGET2=$(echo "$TARGET" | sed 's/12356.101.14.4.3.1.3.1.16/12356.101.14.4.4.1.8.1.16/g')
+			echo "TARGET2=$TARGET2"
+			UPTIME=$(snmpget -Ov -v2c -c $SNMPSTRING $FWHOST $TARGET2 | awk '{printf($3" "$4" "$5)}')
+			echo "UPTIME=$UPTIME"
+			TARGET2=$(echo "$TARGET" | sed 's/12356.101.14.4.3.1.3.1.16/12356.101.14.4.4.1.20.1.16/g')
+			LOAD=$(snmpget -Oqv -v2c -c $SNMPSTRING $FWHOST $TARGET2)
+			;;
 	esac
 	echo "$XTIME up $UPTIME, 0 users, load=$LOAD%" >> $MSG
+	if [ "$DEBUG" = "1" ]; then
+		echo
+		cat $MSG
+	fi
 	$BB $BBDISP "@" < $MSG
  	rm $MSG
 done
-
